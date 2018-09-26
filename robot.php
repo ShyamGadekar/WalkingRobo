d4028ecc398910e087bd14278b97922d0c1c09e6
ShaymGadekar <dev.shyamgadekar@gmail.com>
2018-09-26 10:43:50 +0530

    Some changes

--- robot.php
+++ robot.php
@@ -1,195 +1,64 @@
-<?php 
-    if( !class_exists('botClass') ) :
-        class botClass {
-            private $x, $y, $running ,$walk, $count_walk, $direction, $status;
-            public $command, $debug = false;
+<?php
+const NORTH = 1;
+const EAST = 2;
+const SOUTH = 3;
+const WEST = 4;
+$direction = [ 1 => "NORTH", 2 => "EAST", 3 => "SOUTH", 4 => "WEST"];
+const MULTIPLIER_1 = 1;
+const MULTIPLIER_2 = 1;
+const MULTIPLIER_3 = -1;
+const MULTIPLIER_4 = -1;
 
-            public function __construct(){
-             
+$x = $argv[1];
+$y = $argv[2];
+
+if(!is_numeric($x) || !is_numeric($y)){
+    die("\nOnly Integer value allowed\n");
+}
+
+$currentDirction = $argv[3];
+
+if($currentDirction != 'NORTH' && $currentDirction != 'EAST' && $currentDirction != 'SOUTH' && $currentDirction != 'WEST'){
+     die("\nDirection is not specified\n");
+}
+
+$currentDirctionNumber = constant($currentDirction);
+$path = $argv[4];
+
+//5 2 SOUTH RW2LW4R
+
+for($i = 0; $i < strlen($path); $i++ ){
+    switch($path{$i}){
+        case 'R':
+            if($currentDirctionNumber == 4){
+                $currentDirctionNumber = 1;
+            } else {
+                $currentDirctionNumber++;
             }
-
-            public function init(){
-                
-                $this->reset();
-                $this->validate();
-                $this->process();
-                $this->response();
-                $this->loop();
-            }
-
-            private function reset(){
-
-                $this->command = strtoupper($this->command);
-                $this->walk = 0;
-                $this->x = 0;
-                $this->y = 0;    
-                $this->direction = "North";
-                $this->running = 'Stop';
-                $this->count_walk = 0;
-                $this->status = 1;
-                $this->exit = false;
-                $this->validate = false;
-            }
-
-            private function validate(){
-
-                if( $this->command != '' ) :
-                    if( !( preg_match("/^[LRW0-9]+$/", $this->command) == 1 ) ) :
-                        if( $this->debug ) :
-                            echo "Invalid character.\n"; 
-                        endif;
-                    else:
-                        if( preg_match_all('/W+/', $this->command, $matches_walk) ) :
-                            if( preg_match_all('/\d+/', $this->command, $matches_count_walk) ) :
-                                $this->count_walk = $matches_count_walk[0];
-                                if( count($matches_walk[0]) == count($this->count_walk)  ) :
-                                    $this->validate = true;
-                                else :
-                                    if( $this->debug ) :
-                                        echo "Error type walk but specify number step.\n";
-                                    endif;
-                                    
-                                endif;
-                            else :
-                                if( $this->debug ) :
-                                    echo "Error type walk but specify number step.\n";
-                                endif;
-                            endif;
-                        elseif( preg_match_all('/\d+/', $this->command) ):
-                            if( $this->debug ) :
-                                echo "Error type number step but specify walk.\n";
-                            endif;
-                        else :
-                            $this->validate = true;
-                        endif;
-                    endif; 
-                else :
-                    $this->validate = true;
-                endif;
-            }
-
-            private function process(){
-
-                if( $this->validate ) :
-                    if( $this->debug ) :
-                        echo "From Command : ".$this->command."\n";
-                       
-                    endif;
-
-                    foreach ( str_split($this->command) as $state_value) :
-                        if ( in_array($state_value, ['R', 'L']) ) :
-                            $this->running = $state_value;
-                            $this->direction($state_value);
-
-                        elseif( $state_value == "W" ):
-                            $this->walk();
-                        endif;
-                    endforeach;
-                endif;
-            }
-
-            private function direction(){
-                if( $this->debug ) :
-                    echo $this->status.".) Turne ".$this->running." Move Direction From ".$this->direction;
-                endif;
-
-                if( $this->running === "R" ) :
-                    switch ($this->direction) :
-                        case 'North':
-                            $this->direction = 'East';
-                            break;
-                        case 'East':
-                            $this->direction = 'South';
-                            break;
-                        case 'South':
-                            $this->direction = 'West';
-                            break;
-                        case 'West':
-                            $this->direction = 'North';
-                            break;
-                        default:
-                            break;
-                    endswitch;
-                else :
-                    switch ($this->direction) :
-                        case 'North':
-                            $this->direction = 'West';
-                            break;
-                        case 'East':
-                            $this->direction = 'North';
-                            break;
-                        case 'South':
-                            $this->direction = 'East';
-                            break;
-                        case 'West':
-                            $this->direction = 'South';
-                            break;
-                        default:
-                            break;
-                    endswitch;
-                endif;
-                if( $this->debug ) :
-                    echo " to ".$this->direction."\n";
-                endif;
-                $this->status += 1;
-            }
-
-            private function walk(){
-                switch ($this->direction) :
-                    case 'North':
-                        $this->y += $this->count_walk[$this->walk];
+            break;
+                case 'L':
+                        if($currentDirctionNumber == 1){
+                                $currentDirctionNumber = 4;
+                        } else {
+                                $currentDirctionNumber--;
+                        }
                         break;
-                    case 'East':
-                        $this->x += $this->count_walk[$this->walk];
-                        break;
-                    case 'South':
-                        $this->y -= $this->count_walk[$this->walk];
-                        break;
-                    case 'West':
-                        $this->x -= $this->count_walk[$this->walk];
-                        break;
-                    default:
-                        break;
-                endswitch;
-                if( $this->debug ) :
-                    echo $this->status.".) Walk ".$this->count_walk[$this->walk]." Step \n";
-                endif;
-                $this->walk += 1;
-                $this->status += 1;
+                case 'W':
+            if( !($currentDirctionNumber % 2) ){
+                $x += ($path{$i+1} * constant("MULTIPLIER_".$currentDirctionNumber) );
+            } else {
+                $y += ($path{$i+1} * constant("MULTIPLIER_".$currentDirctionNumber) );
             }
-
-            private function response(){
-                if( $this->validate ) :
-                    echo "\nX : ".$this->x.' Y : '.$this->y." \n";
-                    echo 'Direction : '.$this->direction."\n";
-                else :
-                    echo "This command is syntax error.\n";
-                endif;
+            $i++;
+                        break;
+        default:
+            if(is_numeric($path{$i})){
+                echo "\nWalk ranging from 0 - 9\n";
+            } else {
+                echo "\nProvided char '".$path{$i}."' is not valid\n";
             }
-
-            private function loop(){
-                do {
-                    print "\n exit : ";
-                    print "\nCommand : ";
-                    $command = trim(fgets(STDIN));
-
-                    if( $command == "exit" ) :
-                        
-                        $this->exit = true;
-                        exit(0);
-                    endif;
-                    $this->command = strtoupper($command);
-                    $this->init();
-                } while(!$this->exit);
-            }
-        }
-    endif; 
-    
-    $objBot = new botClass();
-    $objBot->command = isset($argv[1]) ? $argv[1] : '';
-    $objBot->debug = (!getopt("d", [], null)) ? true : false;
-    $objBot->init();
-
+            break;
+    }
+}
+echo $x." ".$y." ".$direction[$currentDirctionNumber]."\n";
 ?>
-
-    
